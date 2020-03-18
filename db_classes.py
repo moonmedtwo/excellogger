fda89a1e52377ba8c3db383624280aa25da4fd49
anhvudl <vu.quoc.anh.ee@gmail.com>
2020-03-18 20:23:30 +0700

    database file

--- db_classes.py
+++ db_classes.py
@@ -1,8 +1,10 @@
 from dataclasses import dataclass
 
-TEXT_1ST_COL = 'Inward code'
-TEXT_2ND_COL = 'Name + Time'
+TEXT_1ST_ROW_1ST_COL = 'Inward code'
+TEXT_1ST_ROW_2ND_COL = 'Name + Time'
 
+IWCODE = 0
+LOGSTRING = 1
 # how the data is formatted:
 # mr.A 1:00, mr.A 1:01, mr.B 1:02
 # mr.A 1:00, mr.B 1:02
