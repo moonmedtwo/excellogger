6cc26e611628339ffac28e9528448d69f1702be6
anhvudl <vu.quoc.anh.ee@gmail.com>
2020-03-18 18:52:03 +0700

    Basic API for logger, TODO: build class

--- excellogger.py
+++ excellogger.py
@@ -1,1 +1,43 @@
-print('Hello')
+import pandas as pd
+from db_classes import LogData, TEXT_1ST_COL, TEXT_2ND_COL
+from openpyxl import Workbook
+from openpyxl import load_workbook
+import random
+
+FILE = 'Book1.xlsx'
+
+def ReadFile(file):
+    df = pd.read_excel(file)
+    return df
+
+def CreateNew(file):
+    workbook = Workbook()
+    sheet = workbook.active
+    sheet.append([TEXT_1ST_COL, TEXT_2ND_COL])
+    workbook.save(filename=file)
+    return workbook
+
+def AppendFile(file, textList):
+    try:
+        workbook = load_workbook(filename=file)
+    except Exception as e:
+        if(isinstance(e,FileNotFoundError)):
+            print("File not existed, new one will be created")
+            workbook = CreateNew(file)
+        else:
+            print(f'{e} ------> exitting')
+            return
+
+    sheet = workbook.active
+    sheet.append(textList)
+    workbook.save(filename=file)
+
+def EditFile(file, cell, value):
+    workbook = load_workbook(filename=file)
+    sheet = workbook.active
+    sheet[cell] = value
+    workbook.save(filename=file)
+
+tf ='test.xlsx'
+textList = ['test1', 'test2']
+AppendFile(tf,textList)
