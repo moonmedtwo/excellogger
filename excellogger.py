4c7b84ac1070759aa5e51b854267ec4ba84b6b45
anhvudl <vu.quoc.anh.ee@gmail.com>
2020-03-18 20:23:18 +0700

    add test case for single task. TODO: reconstruct if duplicated try on multi task to lock file

--- excellogger.py
+++ excellogger.py
@@ -2,7 +2,7 @@ import pandas as pd
 from db_classes import IWCODE, LOGSTRING, TEXT_1ST_ROW_1ST_COL, TEXT_1ST_ROW_2ND_COL
 from openpyxl import Workbook
 from openpyxl import load_workbook
-import random
+import random, math
 
 FILE = 'Book1.xlsx'
 
@@ -34,7 +34,7 @@ def ParseFileAndCheckDuplicated(file):
 
         datadict[row[IWCODE]] = row[LOGSTRING]
 
-    return datadict, isKeyDuplicated
+    return datadict, isKeyDuplicated, workbook
 
 def AppendFile(file, textList):
     try:
@@ -64,5 +64,38 @@ def EditFile(file, cell, value):
     sheet[cell] = value
     workbook.save(filename=file)
 
+IWCODELIST = ['12378','123162','21321','26452','953251']
+USERLIST = ['avu', 'bce', 'def', 'tof', 'iphone']
 tf ='test.xlsx'
-textList = ['test1', 'test2']
+
+import pdb 
+def LogEntry():
+    code = IWCODELIST[int(math.floor(random.uniform(0,len(IWCODELIST))))]
+    user = USERLIST[int(math.floor(random.uniform(0,len(USERLIST))))]
+    print(f'{code}, {user}')
+    datadict, isDuplicated, workbook = ParseFileAndCheckDuplicated(tf)
+    sheet = workbook.active
+
+    #TODO: reconstruct here to remove all duplicated
+    datalist = list(datadict.items())
+
+    for i in range(0, len(datalist)):
+        if code == datalist[i][IWCODE]:
+           tmp = datalist[i][LOGSTRING]
+           tmp = f'{tmp}, {user}'
+           cell = f'B{i+2}'
+           print(f'Editting cell[{cell}] with {tmp}')
+           sheet[cell] = tmp
+           workbook.save(tf)
+           return
+
+    if(isDuplicated == False):
+        sheet.append([code, user])
+        workbook.save(tf)
+        return
+
+
+for i in range(0,10):
+    LogEntry()
+
+ReadFile(tf)
