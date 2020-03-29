8e3a0685c1f25e01781264a7fdf24694ce16437d
avu <moonmtwo@gmail.com>
2020-03-29 17:58:27 +0700

    second choice of username is current logged-on user

--- user.py
+++ user.py
@@ -8,8 +8,12 @@ class UserInfo:
     username = ''
     def __init__(self, username=''):
         if(len(username) == 0):
-            user_info = win32net.NetUserGetInfo(win32net.NetGetAnyDCName(), win32api.GetUserName(), 2)
-            self.username = user_info["full_name"]
+            try:
+                user_info = win32net.NetUserGetInfo(win32net.NetGetAnyDCName(), win32api.GetUserName(), 2)
+                self.username = user_info["full_name"]
+            except Exception:
+                self.username = os.getenv("USERNAME")
+        print(f'{self.username} is being used')
 
     def GetUserName(self):
         ret = self.username
